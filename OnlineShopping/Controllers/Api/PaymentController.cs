95c883283ebf737935989d7a94cafe3c11bc4023
Mats Egnell <mattegol.egnell@gmail.com>
2018-10-09 21:37:32 +0200

    Add implement payment API

---
+++ OnlineShopping/Controllers/Api/PaymentController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using OnlineShopping.Hubs;
+using OnlineShopping.Models;
+
+namespace OnlineShopping.Controllers.Api
+{
+    [EnableCors("MyPolicy")]
+    [Produces("application/json")]
+    [Route("api")]
+    public class PaymentController : Controller
+    {
+        private IHubContext<ChatHub> HubContext { get; set; }
+
+        public PaymentController(IHubContext<ChatHub> hubcontext)
+        {
+            HubContext = hubcontext;
+        }
+
+        [HttpGet("chain")]
+        public async Task<IActionResult> FullChain()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                var url = new Uri("http://localhost:61820/" + "/chain");
+                var response = await client.GetAsync(url);
+
+                var content = await response.Content.ReadAsStringAsync();
+                var model = new
+                {
+                    chain = new List<Block>(),
+                    length = 0
+                };
+                var data = JsonConvert.DeserializeAnonymousType(content, model);
+
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpPost("Pay")]
+        public async Task<IActionResult> MakePayment([FromBody]Transaction transaction, string ip, int pid)
+        {
+            var json = JsonConvert.SerializeObject(transaction);
+
+            var uri = "http://localhost:12304/transactions/new";
+            var stringContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.PostAsync(uri, stringContent);
+            var content = await response.Content.ReadAsStringAsync();
+            var rsp = new { message = "" };
+            var data = JsonConvert.DeserializeAnonymousType(content, rsp);
+
+            if (data.message.Contains("Transaction will be added to Block") && ip != null)
+            {
+                //successfull and unlock the video
+                await this.HubContext.Clients.All.SendAsync(
+                    ip,
+                    pid,
+                    VideoList.Videos().First(x => x.Id == pid).Url);
+
+                VideoOwned.AddUser(ip, pid);
+            }
+
+            return Ok(data);
+        }
+    }
+}
--- OnlineShopping/Startup.cs
+++ OnlineShopping/Startup.cs
@@ -24,6 +24,13 @@ namespace OnlineShopping
         {
             services.AddMvc();
             services.AddSignalR();
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
