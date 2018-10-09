19cfda149ebea04ceac27cd57b5c9ac02c443c18
Mats Egnell <mattegol.egnell@gmail.com>
2018-10-09 20:54:35 +0200

    Add video unlocking page

--- OnlineShopping/Controllers/HomeController.cs
+++ OnlineShopping/Controllers/HomeController.cs
@@ -4,16 +4,38 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using OnlineShopping.Hubs;
 using OnlineShopping.Models;
 
 namespace OnlineShopping.Controllers
 {
     public class HomeController : Controller
     {
+        private IHubContext<ChatHub> HubContext { get; set; }
+
+        public HomeController(IHubContext<ChatHub> hubcontext)
+        {
+            HubContext = hubcontext;
+            //https://stackoverflow.com/questions/46904678/call-signalr-core-hub-method-from-controller
+        }
+
         public IActionResult Index()
         {
+            var videoList = VideoList.Videos();
+            ViewBag.Videos = videoList;
+
             return View();
         }
+
+        public async Task<IActionResult> ApiCall(string ip, int id)
+        {
+            await this.HubContext.Clients.All.SendAsync(ip, id, VideoList.Videos().First(x => x.Id == Convert.ToInt32(id)).Url);
+            VideoOwned.AddUser(ip, id);
+
+            return Content("successfull");
+        }
+
 
         public IActionResult Error()
         {
