349fb7b0fe9924fc60cab150fa6c180fb1e7c7dc
Mats Egnell <mattegol.egnell@gmail.com>
2018-10-09 19:44:58 +0200

    Add implement online shopping site

---
+++ OnlineShopping/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineShopping.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
