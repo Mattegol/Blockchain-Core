4fd8088279d7d0080ac64c2c02db138e5439a0db
Mats Egnell <mattegol.egnell@gmail.com>
2018-10-10 13:09:33 +0200

    Add mobile wallet development using Xamarin

---
+++ Wallet/Wallet/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Wallet.Views;
+using Xamarin.Forms;
+
+namespace Wallet
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Wallet/Wallet/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Wallet.Models;
+
+namespace Wallet.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
